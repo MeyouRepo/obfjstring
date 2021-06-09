abe0f86db66937d0132b79e5f60e19fa55cf3296
邓捷w99503 <99503@sangfor.com>
2021-06-09 12:02:10 +0800

    Can be null

--- src/main/java/me/liangchengj/obfjstring/StringFieldOfClass.kt
+++ src/main/java/me/liangchengj/obfjstring/StringFieldOfClass.kt
@@ -2,8 +2,8 @@ package me.liangchengj.obfjstring
 
 
 data class StringFieldOfClass(
-    var name: String,
-    var value: String
+    var name: String?,
+    var value: String?
 ) {
     companion object {
         const val STRING_SIG: String = "Ljava/lang/String;"
