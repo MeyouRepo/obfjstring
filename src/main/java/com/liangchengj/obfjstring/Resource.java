31f33d8fa7be90cac55d90c8075d0d7ca880cc2c
邓捷w99503 <99503@sangfor.com>
2021-06-09 18:39:41 +0800

    API

---
+++ src/main/java/com/liangchengj/obfjstring/Resource.java
@@ -0,0 +1,36 @@
+package com.liangchengj.obfjstring;
+
+import com.liangchengj.obfjstring.io.Stream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.Enumeration;
+
+/**
+ * Created at 2021/6/9 18:33.
+ *
+ * @author Liangcheng Juves
+ */
+public final class Resource {
+
+  private Resource() {}
+
+  private static final ClassLoader classLoader = Resource.class.getClassLoader();
+
+  public static InputStream getAsStream(String name) {
+    return classLoader.getResourceAsStream(name);
+  }
+
+  public static URL get(String name) {
+    return classLoader.getResource(name);
+  }
+
+  public static Enumeration<URL> getAsEnumeration(String name) throws IOException {
+    return classLoader.getResources(name);
+  }
+
+  public static void readAndWrite(String name, OutputStream outputStream) {
+    Stream.readAndWrite(getAsStream(name), outputStream);
+  }
+}
