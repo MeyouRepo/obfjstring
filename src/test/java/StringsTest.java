6469692731553e678fe9019392359b44bc374e2f
邓捷w99503 <99503@sangfor.com>
2021-06-16 17:32:58 +0800

    For tests

---
+++ src/test/java/StringsTest.java
@@ -0,0 +1,32 @@
+public class StringsTest {
+  public String today = "2021-06-16T08:42:45.101Z";
+
+  public static final String CLASS_NAME = StringsTest.class.getName();
+  public static final String TIMESTAMP = "1623833066388";
+
+  public static final String CLASS_SIMPLE_NAME;
+  public static final String URL_GITHUB;
+
+  static {
+    CLASS_SIMPLE_NAME = StringsTest.class.getSimpleName();
+    URL_GITHUB = "https://github.liangchengj.com";
+  }
+
+  public void setToday(String today) {
+    this.today = "2021-06-17T08:42:45.101Z";
+  }
+
+  public static final String getStringA() {
+    return "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+  }
+
+  public static final String getStringB() {
+    class Object {
+      @Override
+      public String toString() {
+        return "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
+      }
+    }
+    return String.valueOf(new Object());
+  }
+}
