7763eed5c762bd8793d18f42b914234fe69d7487
Liangcheng Juves <email@lcjuves.com>
2025-04-16 11:18:57 +0800

    Revert "The key used to encrypt the obfuscated string is added with a secure random number"
    This reverts commit f522501e96169aadee8aff8a5acca332f50bf306

--- src/main/java/com/lcjuves/obfjstring/JavaStringObfuscator.java
+++ src/main/java/com/lcjuves/obfjstring/JavaStringObfuscator.java
@@ -1,7 +1,6 @@
 package com.lcjuves.obfjstring;
 
 import java.nio.charset.StandardCharsets;
-import java.security.SecureRandom;
 import java.util.UUID;
 
 /**
@@ -11,45 +10,44 @@ import java.util.UUID;
  */
 public final class JavaStringObfuscator {
 
-    public static final String JAVA_CLASS_FILE_EXT = ".class";
+  public static final String JAVA_CLASS_FILE_EXT = ".class";
 
-    private JavaStringObfuscator() {}
+  private JavaStringObfuscator() {}
 
-    public static byte[] rsaEncrypt(String string, RSA.PublicKey publicKey) {
-        return RSA.encrypt(string.getBytes(StandardCharsets.UTF_8), publicKey);
+  public static byte[] rsaEncrypt(String string, RSA.PublicKey publicKey) {
+    return RSA.encrypt(string.getBytes(StandardCharsets.UTF_8), publicKey);
+  }
+
+  public static String rsaDecrypt(byte[] bytes, RSA.PrivateKey privateKey) {
+    return new String(RSA.decrypt(bytes, privateKey), StandardCharsets.UTF_8);
+  }
+
+  public static byte[] aesEncrypt(String string, String key, String iv) {
+    return AES.encrypt(string.getBytes(StandardCharsets.UTF_8), key, iv);
+  }
+
+  public static String aesDecrypt(byte[] bytes, String key, String iv) {
+    return new String(AES.decrypt(bytes, key, iv), StandardCharsets.UTF_8);
+  }
+
+  public static String getJNIStyleClassName(Class<?> clazz) {
+    return clazz.getName().replace('.', '/');
+  }
+
+  public static String getJNIStyleShortClassName(Class<?> clazz) {
+    String jniStyleClassName = getJNIStyleClassName(clazz);
+    return jniStyleClassName.substring(jniStyleClassName.lastIndexOf('/') + 1);
+  }
+
+  public static String genUUIDForAESKey() {
+    return genUUID(16);
+  }
+
+  public static String genUUID(int len) {
+    String uuid = UUID.randomUUID().toString().replace("-", "");
+    if (len <= 0) {
+      return uuid;
     }
-
-    public static String rsaDecrypt(byte[] bytes, RSA.PrivateKey privateKey) {
-        return new String(RSA.decrypt(bytes, privateKey), StandardCharsets.UTF_8);
-    }
-
-    public static byte[] aesEncrypt(String string, String key, String iv) {
-        return AES.encrypt(string.getBytes(StandardCharsets.UTF_8), key, iv);
-    }
-
-    public static String aesDecrypt(byte[] bytes, String key, String iv) {
-        return new String(AES.decrypt(bytes, key, iv), StandardCharsets.UTF_8);
-    }
-
-    public static String getJNIStyleClassName(Class<?> clazz) {
-        return clazz.getName().replace('.', '/');
-    }
-
-    public static String getJNIStyleShortClassName(Class<?> clazz) {
-        String jniStyleClassName = getJNIStyleClassName(clazz);
-        return jniStyleClassName.substring(jniStyleClassName.lastIndexOf('/') + 1);
-    }
-
-    public static String genUUIDForAESKey() {
-        SecureRandom secureRandom = new SecureRandom();
-        return genUUID(16) + secureRandom.nextLong();
-    }
-
-    public static String genUUID(int len) {
-        String uuid = UUID.randomUUID().toString().replace("-", "");
-        if (len <= 0) {
-            return uuid;
-        }
-        return uuid.substring(0, len);
-    }
+    return uuid.substring(0, len);
+  }
 }
