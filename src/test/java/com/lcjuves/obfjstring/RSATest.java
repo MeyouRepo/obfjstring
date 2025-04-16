c8698bcbe0fd06bd3f4897ec49d25c744dde2ae4
Liangcheng Juves <email@lcjuves.com>
2025-04-16 03:50:24 +0000

    Update RSA encryption, decryption and key pair related implementation

--- src/test/java/com/lcjuves/obfjstring/RSATest.java
+++ src/test/java/com/lcjuves/obfjstring/RSATest.java
@@ -2,18 +2,28 @@ package com.lcjuves.obfjstring;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class RSATest {
 
-  private static final RSA.KeyPair keypair = RSA.genKeyPair();
-  private static final String string = "Liangcheng Juves";
+    private static final RSA.KeyPair keypair = RSA.genKeyPair();
+    private static final String string = "Liangcheng Juves";
 
-  @Test
-  void test() {
-    String encrypted = new String(RSA.encrypt(string.getBytes(), keypair.getPublicKey()));
-    System.out.println(encrypted);
+    @Test
+    void test() {
+        String encrypted = new String(RSA.encrypt(string.getBytes(), keypair.getPublicKey()));
+        System.out.println(encrypted);
 
-    String decrypted = new String(RSA.decrypt(encrypted.getBytes(), keypair.getPrivateKey()));
+        String decrypted = new String(RSA.decrypt(encrypted.getBytes(), keypair.getPrivateKey()));
+        System.out.println(decrypted);
+        assertEquals(string, decrypted);
 
-    System.out.println(decrypted);
-  }
+        String signed = new String(RSA.sign(string.getBytes(), keypair.getPrivateKey()));
+        System.out.println(signed);
+
+        boolean verifyResult = RSA.verify(string.getBytes(), signed.getBytes(), keypair.getPublicKey());
+        System.out.println(verifyResult);
+        assertTrue(verifyResult);
+    }
 }
